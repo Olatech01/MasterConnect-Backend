4ae1ae1fd8e853dbe1634f66f42dfe69186b4796
unknown <mustaphas902@gmail.com>
2024-11-25 12:36:58 +0100

    adjustment

--- Route/handler.js
+++ Route/handler.js
@@ -13,8 +13,86 @@ const {
     getJobById,
 } = require("../controller/JobController");
 const { uploadCandidateDetails } = require("../controller/candidateProfile");
-
+const multer = require("multer")
+const path = require("path");
+const CandidateProfile = require("../Model/registerAsCandidate");
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null,
+            Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage:
+        storage
+}).fields([
+    { name: 'profilePicture', maxCount: 1 },
+    { name: 'document', maxCount: 1 },
+    { name: 'certificate', maxCount: 1 }
+]);
+
+
+router.post("/candidateRegistration", upload, async (req, res) => {
+    try {
+        const {
+            candidateName,
+            candidateEmail,
+            candidateAge,
+            candidateGender,
+            candidatePosition,
+            candidateLinkedin,
+            internShipCompleted,
+            institution,
+            degreeType,
+            institutionStartDate,
+            institutionEndDate,
+            departmentType,
+            hobbies,
+            rank,
+        } = req.body;
+        const passportPath = req.files.passport[0].path;
+        const governmentIDPath = req.files.governmentID[0].path;
+        const collegeIDPath = req.files.collegeID[0].path;
+        const certificatePath = req.files.certificate[0].path;
+
+
+
+        const profile = new CandidateProfile({
+            candidateName,
+            candidateEmail,
+            candidateAge,
+            candidateGender,
+            candidatePosition,
+            candidateLinkedin,
+            internShipCompleted,
+            institution,
+            degreeType,
+            institutionStartDate,
+            institutionEndDate,
+            departmentType,
+            hobbies,
+            rank,
+            passport: passportPath,
+            governmentID: governmentIDPath,
+            collegeID: collegeIDPath,
+            certificate: certificatePath,
+        });
+
+        await profile.save();
+        res.status(201).json({ message: 'Candidate details registered successfully', profile });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Server Error'
+        });
+    }
+})
 
 // User Routes
 router.route("/register").post(register);
@@ -30,6 +108,8 @@ router.route("/singlejob/:id").get(getJobById);
 
 // Candidate Routes
 router.route("/candidate").post(uploadCandidateDetails);
+
+
 
 
 
